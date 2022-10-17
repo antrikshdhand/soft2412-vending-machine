5f495bfa4873253e76d6d652597f4501cff4cc21
akap2114 <akap2114@uni.sydney.edu.au>
2022-10-17 22:32:53 +1100

    Working stub

--- app/src/main/java/VendingMachine/App.java
+++ app/src/main/java/VendingMachine/App.java
@@ -1,9 +1,12 @@
 package VendingMachine;
 
-import javafx.application.*;
-import javafx.event.*;
-import javafx.scene.*;
-import javafx.stage.*;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 public class App extends Application {
     /**
