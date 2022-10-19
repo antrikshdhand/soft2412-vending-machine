33c96ed157ceb8a94f1e63f10e8c9182ffded5e1
Nemo <nemogage@gmail.com>
2022-10-20 00:53:24 +1100

    Comment broken files

--- app/src/main/java/VendingMachine/CashierPortal.java
+++ app/src/main/java/VendingMachine/CashierPortal.java
@@ -1,86 +1,86 @@
-package VendingMachine;
-
-import java.util.ArrayList;
-
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
-import javafx.scene.text.Font;
-
-public class CashierPortal extends Page {
-
-    ArrayList<Button> buttons = new ArrayList<Button>();
-
-    private Pane pane;
-
-    public CashierPortal(App app) {
-        
-        pane = new StackPane();
-        scene = new Scene(pane, WIDTH, HEIGHT);
-
-        VBox box = new VBox();
-        box.setSpacing(SPACING);
-        box.setPrefWidth(PREFWIDTH);
-        box.setAlignment(Pos.CENTER);
-
-        // Create Buttons
-        Button modifyAvailableCash = new Button();
-        Button generateSummaryOfChange = new Button();
-        Button generateSummaryOfTransaction = new Button();
-        Button returnButton = new Button();
-
-        // Add to button array
-        buttons.add(modifyAvailableCash);
-        buttons.add(generateSummaryOfChange);
-        buttons.add(generateSummaryOfTransaction);
-        buttons.add(returnButton);
-
-        // Set universal values
-        for (Button button : buttons) {
-            button.setMinWidth(box.getPrefWidth());
-        }
-
-        // Setting text to the button
-        modifyAvailableCash.setText("Modify Available Cash");
-        generateSummaryOfChange.setText("Generate Summary of Change");
-        generateSummaryOfTransaction.setText("Generate Summary of Transaction");
-        returnButton.setText("Return to Default Page");
-
-        returnButton.setOnAction(e -> {
-            app.switchScenes(app.getSceneManager().getDeafultPageScene());
-        });
-
-        // Setting the location of the button
-        modifyAvailableCash.setTranslateX(150);
-        modifyAvailableCash.setTranslateY(60);
-
-        generateSummaryOfChange.setTranslateX(150);
-        generateSummaryOfChange.setTranslateY(60);
-
-        generateSummaryOfTransaction.setTranslateX(150);
-        generateSummaryOfTransaction.setTranslateY(60);
-
-        returnButton.setTranslateX(150);
-        returnButton.setTranslateY(60);
-
-        // Add objects to pane
-        for (Button button : buttons) {
-            pane.getChildren().add(button);
-        }
-
-    }
-
-    public Scene getScene() {
-        return scene;
-    }
-}
+// package VendingMachine;
+//
+// import java.util.ArrayList;
+//
+// import javafx.application.Application;
+// import javafx.event.ActionEvent;
+// import javafx.event.EventHandler;
+// import javafx.scene.Scene;
+// import javafx.scene.control.Button;
+// import javafx.scene.layout.StackPane;
+// import javafx.stage.Stage;
+// import javafx.geometry.Insets;
+// import javafx.geometry.Pos;
+// import javafx.scene.layout.Pane;
+// import javafx.scene.layout.VBox;
+// import javafx.scene.text.FontWeight;
+// import javafx.scene.text.Text;
+// import javafx.scene.text.Font;
+//
+// public class CashierPortal extends Page {
+//
+//     ArrayList<Button> buttons = new ArrayList<Button>();
+//
+//     private Pane pane;
+//
+//     public CashierPortal(App app) {
+//
+//         pane = new StackPane();
+//         scene = new Scene(pane, WIDTH, HEIGHT);
+//
+//         VBox box = new VBox();
+//         box.setSpacing(SPACING);
+//         box.setPrefWidth(PREFWIDTH);
+//         box.setAlignment(Pos.CENTER);
+//
+//         // Create Buttons
+//         Button modifyAvailableCash = new Button();
+//         Button generateSummaryOfChange = new Button();
+//         Button generateSummaryOfTransaction = new Button();
+//         Button returnButton = new Button();
+//
+//         // Add to button array
+//         buttons.add(modifyAvailableCash);
+//         buttons.add(generateSummaryOfChange);
+//         buttons.add(generateSummaryOfTransaction);
+//         buttons.add(returnButton);
+//
+//         // Set universal values
+//         for (Button button : buttons) {
+//             button.setMinWidth(box.getPrefWidth());
+//         }
+//
+//         // Setting text to the button
+//         modifyAvailableCash.setText("Modify Available Cash");
+//         generateSummaryOfChange.setText("Generate Summary of Change");
+//         generateSummaryOfTransaction.setText("Generate Summary of Transaction");
+//         returnButton.setText("Return to Default Page");
+//
+//         returnButton.setOnAction(e -> {
+//             app.switchScenes(app.getSceneManager().getDeafultPageScene());
+//         });
+//
+//         // Setting the location of the button
+//         modifyAvailableCash.setTranslateX(150);
+//         modifyAvailableCash.setTranslateY(60);
+//
+//         generateSummaryOfChange.setTranslateX(150);
+//         generateSummaryOfChange.setTranslateY(60);
+//
+//         generateSummaryOfTransaction.setTranslateX(150);
+//         generateSummaryOfTransaction.setTranslateY(60);
+//
+//         returnButton.setTranslateX(150);
+//         returnButton.setTranslateY(60);
+//
+//         // Add objects to pane
+//         for (Button button : buttons) {
+//             pane.getChildren().add(button);
+//         }
+//
+//     }
+//
+//     public Scene getScene() {
+//         return scene;
+//     }
+// }
--- app/src/main/java/VendingMachine/OwnerPortal.java
+++ app/src/main/java/VendingMachine/OwnerPortal.java
@@ -1,92 +1,88 @@
-package VendingMachine;
+ package VendingMachine;
 
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-import javafx.scene.text.Font;
+ import javafx.application.Application;
+ import javafx.geometry.Insets;
+ import javafx.geometry.Pos;
+ import javafx.scene.Scene;
+ import javafx.scene.control.Button;
+ import javafx.scene.layout.Pane;
+ import javafx.scene.layout.StackPane;
+ import javafx.scene.layout.VBox;
+ import javafx.scene.text.FontWeight;
+ import javafx.scene.text.Text;
+ import javafx.stage.Stage;
+ import javafx.scene.text.Font;
 
-import javax.swing.plaf.ViewportUI;
-import java.awt.*;
+ import javax.swing.plaf.ViewportUI;
+ import java.awt.*;
 
-public class OwnerPortal extends Page{
-
-
-    private Pane pane;
-
-    private Button cashierPortal;
-    private Button sellerPortal;
-    private Button manageSCO;
-    private Button summary;
-    private Button cancelledTransactions;
-    private Button returnToDp;
-
-    public OwnerPortal(App app){
-
-        pane = new StackPane();
-        scene = new Scene(pane, WIDTH, HEIGHT);
-
-        VBox box = new VBox();
-        box.setSpacing(5);
-        box.setPrefWidth(190.00);
-        box.setAlignment(Pos.CENTER);
-
-        cashierPortal = new Button("Cashier portal");
-
-        cashierPortal.setOnAction(e -> {
-            app.switchScenes(app.getSceneManager().getCashierPortalScene());
-        });
-
-        sellerPortal = new Button("Seller portal");
-
-        sellerPortal.setOnAction(e -> {
-            app.switchScenes(app.getSceneManager().getSellerPortalScene());
-        });
-
-        manageSCO = new Button("Managed privileged users");
-        summary = new Button("Generate Users Summary");
-        cancelledTransactions = new Button("View unsuccessful transaction");
-        returnToDp = new Button("Return to default page");
-
-        returnToDp.setOnAction(e -> {
-            app.switchScenes(app.getSceneManager().getDeafultPageScene());
-        });
-
-        cashierPortal.setMinWidth(box.getPrefWidth());
-        sellerPortal.setMinWidth(box.getPrefWidth());
-        manageSCO.setMinWidth(box.getPrefWidth());
-        summary.setMinWidth(box.getPrefWidth());
-        cancelledTransactions.setMinWidth(box.getPrefWidth());
-
-        returnToDp.setTranslateX(-550);
-        returnToDp.setTranslateY(320);
-
-        Text title = new Text();
-        title.setText("Owner's Portal");
-        title.setFont(Font.font("Arial", FontWeight.BOLD, 35));
-
-        box.getChildren().addAll(title, sellerPortal, cashierPortal, manageSCO, summary, cancelledTransactions);
-        pane.getChildren().add(box);
-        pane.getChildren().add(returnToDp);
+ public class OwnerPortal extends Page{
 
 
+     private Pane pane;
+
+     private Button cashierPortal;
+     private Button sellerPortal;
+     private Button manageSCO;
+     private Button summary;
+     private Button cancelledTransactions;
+     private Button returnToDp;
+
+     public OwnerPortal(App app){
+
+         pane = new StackPane();
+         scene = new Scene(pane, WIDTH, HEIGHT);
+
+         VBox box = new VBox();
+         box.setSpacing(5);
+         box.setPrefWidth(190.00);
+         box.setAlignment(Pos.CENTER);
+
+         cashierPortal = new Button("Cashier portal");
+
+         cashierPortal.setOnAction(e -> {
+             app.switchScenes(app.getSceneManager().getCashierPortalScene());
+         });
+
+         sellerPortal = new Button("Seller portal");
+
+         sellerPortal.setOnAction(e -> {
+             app.switchScenes(app.getSceneManager().getSellerPortalScene());
+         });
+
+         manageSCO = new Button("Managed privileged users");
+         summary = new Button("Generate Users Summary");
+         cancelledTransactions = new Button("View unsuccessful transaction");
+         returnToDp = new Button("Return to default page");
+
+         returnToDp.setOnAction(e -> {
+             app.switchScenes(app.getSceneManager().getDeafultPageScene());
+         });
+
+         cashierPortal.setMinWidth(box.getPrefWidth());
+         sellerPortal.setMinWidth(box.getPrefWidth());
+         manageSCO.setMinWidth(box.getPrefWidth());
+         summary.setMinWidth(box.getPrefWidth());
+         cancelledTransactions.setMinWidth(box.getPrefWidth());
+
+         returnToDp.setTranslateX(-550);
+         returnToDp.setTranslateY(320);
+
+         Text title = new Text();
+         title.setText("Owner's Portal");
+         title.setFont(Font.font("Arial", FontWeight.BOLD, 35));
+
+         box.getChildren().addAll(title, sellerPortal, cashierPortal, manageSCO, summary, cancelledTransactions);
+         pane.getChildren().add(box);
+         pane.getChildren().add(returnToDp);
 
 
+     }
 
-
-    }
-
-    public Scene getScene(){
-        return this.scene;
-    }
+     public Scene getScene(){
+         return this.scene;
+     }
 
 
 
-}
+ }
