d73a86472303f144e5fce9ef0f3301d11302ff11
Stainima <112240341+Stainima@users.noreply.github.com>
2022-10-20 13:46:11 +1100

    Update Session and hard coded atm

--- app/src/main/java/VendingMachine/Session.java
+++ app/src/main/java/VendingMachine/Session.java
@@ -10,28 +10,67 @@ public class Session {
 
     private final int tickLimit = 120;
 
+
+    /**
+     * Returns the user Name of the session
+     * @return user Name ( String )
+     */
     public String getUserName() {
         return userName;
     }
 
+    /**
+     * Function to return the role of current session user
+     * @return role ( String )
+     */
     public String getRole() {
         return role;
     }
 
+    /**
+     * Function to return the state of the transaction, has it been started or not.
+     * @return whether the transaction has been started or not ( boolean )
+     */
     public boolean getTransactionState(){
         return this.startedTransaction;
     }
 
+    /**
+     * Function to set the state of the transaction.
+     * @param startedTransaction ( Boolean )
+     */
     public void setStartedTransaction(boolean startedTransaction) {
         this.startedTransaction = startedTransaction;
     }
 
+
+    /**
+     * Function to set the role for a session.
+     * @param role ( String )
+     */
     public void setRole(String role) {
         this.role = role;
     }
 
+
+    /**
+     * Function to set the userName for the session.
+     * @param userName ( String )
+     */
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
+
+
+    // todo, it is hardcoded atm.
+
+
+    /**
+     * Function to reset the role and user back to guest.
+     */
+    public void resetSession(){
+        userName = "Guest";
+        role = "G";
+    }
 }
