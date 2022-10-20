cf9c68d23f67e6c55be46eb52e7467404f5fb8c2
Antriksh Dhand <adha5655@uni.sydney.edu.au>
2022-10-20 13:48:14 +1100

    Fix bug

--- app/src/main/java/VendingMachine/Login.java
+++ app/src/main/java/VendingMachine/Login.java
@@ -6,6 +6,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 import VendingMachine.Page;
 import VendingMachine.SceneManager;
@@ -19,15 +24,28 @@ public class Login extends Page {
         this.sceneManager = sceneManager;
 
         GridPane grid = new GridPane();
-        grid.gridLinesVisibleProperty(true);
+        grid.setGridLinesVisible(true);
         
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(0, 10, 0, 10));
         this.scene = new Scene(grid, WIDTH, HEIGHT);
 
-        Label loginLabel = new Label("LOGIN!");
-        grid.add(loginLabel, 3, 3);
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("User Name:");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
 
 
 
