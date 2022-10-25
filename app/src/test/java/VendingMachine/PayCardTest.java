eda15d90fdff6fa8b10ad80b19e4c9ede5a677ed
akap2114 <akap2114@uni.sydney.edu.au>
2022-10-25 11:24:27 +1100

    Create tests for PayCard

---
+++ app/src/test/java/VendingMachine/PayCardTest.java
@@ -0,0 +1,30 @@
+package VendingMachine;
+
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class PayCardTest {
+
+    // simple test for openConn()
+    @Test
+    void openConnTest(){
+
+        assertEquals(-1, value);
+
+    }
+
+}
